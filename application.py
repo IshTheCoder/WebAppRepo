f13025751fdb699ff5ebf01d8bf6ee6a0e3c0d2e
Sophy <sophylee1022@gmail.com>
2020-05-23 15:22:35 +0800

    displayModeBar set to True for app and app2

--- application.py
+++ application.py
@@ -49,7 +49,7 @@ app.layout = html.Div(
                               children=[
                                   html.H2('ECE229-PCA'),
                                   dcc.Graph(
-                                      id='gapminder', config={'displayModeBar': False}
+                                      id='gapminder', config={'displayModeBar': True}
                                   )
                                 # html.H2('2D scatter plot'),
                                 # dcc.Graph(
@@ -96,7 +96,7 @@ app2.layout = html.Div(
                                   # ),
                                 html.H2('2D scatter plot'),
                                 dcc.Graph(
-                                    id='gapminder1', config={'displayModeBar': False}
+                                    id='gapminder1', config={'displayModeBar': True}
                                 )
                               ])  # Define the right element
                  ])
